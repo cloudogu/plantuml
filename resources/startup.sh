7521774bfeaa1a6c8c8a53f8ac199625e792688e
Sebastian Sdorra <sebastian.sdorra@triology.de>
2017-08-25 08:41:41 +0200

    Fixes webui on installation with self signed certificates

---
+++ resources/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# adjust webapp permissions
+chown -R plantuml:plantuml ${CATALINA_BASE}/webapps/
+
+# create truststore, which is used in the setenv.sh
+create_truststore.sh > /dev/null
+
+# startup tomcat
+exec su - plantuml -c "${CATALINA_SH} run"
