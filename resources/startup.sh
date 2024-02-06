7d7d30fbf835716dd8b484cf2dcc4814c30f11ce
Philipp Pixel <boris.schaa@cloudogu.com>
2024-02-06 16:59:42 +0100

    #16 Add cloudogu logo as restart marker

--- resources/startup.sh
+++ resources/startup.sh
@@ -11,7 +11,22 @@ if [[ ${sourcingExitCode} -ne 0 ]]; then
   echo "ERROR: An error occurred while sourcing /logging.sh."
 fi
 
+printCloudoguLogo() {
+  echo "                                     ./////,                    "
+  echo "                                 ./////==//////*                "
+  echo "                                ////.  ___   ////.              "
+  echo "                         ,**,. ////  ,////A,  */// ,**,.        "
+  echo "                    ,/////////////*  */////*  *////////////A    "
+  echo "                   ////'        \VA.   '|'   .///'       '///*  "
+  echo "                  *///  .*///*,         |         .*//*,   ///* "
+  echo "                  (///  (//////)**--_./////_----*//////)   ///) "
+  echo "                   V///   '°°°°      (/////)      °°°°'   ////  "
+  echo "                    V/////(////////\. '°°°' ./////////(///(/'   "
+  echo "                       'V/(/////////////////////////////V'      "
+}
+
 runMain() {
+  printCloudoguLogo
 
   # create truststore, which is used in the setenv.sh
   create_truststore.sh "${TRUSTSTORE}" >/dev/null
