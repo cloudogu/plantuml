b68a0d875cd05afd6676d6967a20af9eea8b9f38
Dennis Schwarzer <dennis.schwarzer@cloudogu.com>
2025-06-03 13:10:22 +0200

    [#42] trying to fix flakey int tests

--- integrationTests/cypress/support/step_definitions/given.ts
+++ integrationTests/cypress/support/step_definitions/given.ts
@@ -1,6 +1,7 @@
 import {Given} from '@badeball/cypress-cucumber-preprocessor';
 
 Given('diagram type is set to ascii', () => {
+    cy.clickWarpMenuCheckboxIfPossible();
     cy.get('#btn-settings').click();
     // wait so settings json gets loaded
     cy.wait(2500);
--- integrationTests/cypress/support/step_definitions/when.ts
+++ integrationTests/cypress/support/step_definitions/when.ts
@@ -1,10 +1,12 @@
 import {When} from '@badeball/cypress-cucumber-preprocessor';
 
 When('diagram gets edited', () => {
+    cy.clickWarpMenuCheckboxIfPossible();
     cy.get('.view-line').eq(1).click({ force: true }).type('{enter}Alice -> Bob : test {enter}Carl -> Carl : myself');
 });
 
 When('diagram gets exported', () => {
+    cy.clickWarpMenuCheckboxIfPossible();
     cy.get('.editor-menu').click({ force: true });
     cy.get('.menu-item').eq(2).click({ force: true });
     cy.get('#diagram-export-ok-btn').click({ force: true });
@@ -13,6 +15,7 @@ When('diagram gets exported', () => {
 });
 
 When('picture link gets clicked', () => {
+    cy.clickWarpMenuCheckboxIfPossible();
     cy.get('#url').invoke('val').then((value) => {
         cy.request('/' + Cypress.env('DoguName') + '/' + value, {failOnStatusCode: false}).then((res) => {
             cy.log(res.body);
